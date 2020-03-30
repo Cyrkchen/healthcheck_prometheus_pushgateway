25bd0013706f28b7c4bc74d01b0f184d69ffd7a1
Cyrkchen <cyrkchen@gmail.com>
2020-03-30 13:58:50 +0800

    add get urls method - from web

--- health_metrics.sh
+++ health_metrics.sh
@@ -6,8 +6,14 @@ script_logging_level="INFO"
 logThis() {
     local log_message=$1
     local log_priority=$2
+
+    #check if level exists
     [[ ${levels[$log_priority]} ]] || return 1
+
+    #check if level is enough
     (( ${levels[$log_priority]} < ${levels[$script_logging_level]} )) && return 2
+
+    #log here
     echo "$(date) - ${log_message}"
 }
 
@@ -15,11 +21,14 @@ logThis() {
 function show_help {
    echo "\
    -p Pushgateway url like http://127.0.0.1:9091/metrics/job/healthcheck
-   -u Check URLS like www.google.com,www.yahoo.com
+   -u Check URLS like https://www.google.com,https://www.yahoo.com
+   -w Get URLS from WEB like 'curl http://www.example.com'
+      https://www.google.com
+      https://www.yahoo.com
    "
 }
 
-while getopts "h?p:u:" opt; do
+while getopts "h?p:u:w:" opt; do
     case "$opt" in
     h|\?)
         show_help
@@ -29,34 +38,29 @@ while getopts "h?p:u:" opt; do
         ;;
     u)  URLS=(${OPTARG//,/ })
         ;;
+    w)  WEB_URLS_URI=${OPTARG}
+        ;;
     *)  show_help
         exit 0
     esac
 done
 
-if [ ${URLS_URI}];then
-    ### get reponse "http://xxx.yyy.zzz http://aaa.bbb.ccc http://xxx1.yyy1.zzz1"
-    URLS=($(curl ${URLS_URI}))
-else
-    URLS=(${URLS//,/ })
-fi
-if [ ! ${CURL_CONNECT_TIMEOUT} ];then
-    CURL_CONNECT_TIMEOUT=10
-fi
-if [ ! ${CURL_MAX_TIME} ];then
-    CURL_MAX_TIME=10
-fi
-
 BODY=()
 METRICS_NAME="healthcheck_http_request_duration_seconds"
-STATUS_CODE_NAME="healthcheck_http_status_code"
-for url in "${URLS[@]}"
+
+if [ ${WEB_URLS_URI} ]; then
+   URLS_TEMP=$(curl ${WEB_URLS_URI})
+   if [[ ${URLS_TEMP} =~ .*http.* ]]; then
+        URLS+=( "${URLS_TEMP[@]//\\n/ }" )
+   fi
+fi
+
+# dedup URLS
+for url in $(echo "${URLS[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' ')
 do
     logThis "start curl ${url}"
-    health_metrics=$(curl --connect-timeout ${CURL_CONNECT_TIMEOUT}  --max-time ${CURL_MAX_TIME} -o /dev/null -s -w "%{time_total},%{http_code}" ${url})
-    health_metric=(${health_metrics//,/ })
-    health_duration_metric=${health_metric[0]}
-    health_status_code=${health_metric[1]}
+    health_metric=$(curl --connect-timeout ${CURL_CONNECT_TIMEOUT:-10}  --max-time ${CURL_MAX_TIME:-10} -o /dev/null -s -w "%{time_total}" ${url})
+    health_metric=$(printf %.10f "${health_metric}")
     domain_name=(${url//\// })
     if [[ "${domain_name[0]}" == *"http"* ]];then
         url_temp=${domain_name[1]}
@@ -66,12 +70,10 @@ do
     url_temp1=${url_temp//./_}
     url_temp1=${url_temp1//:/_}
     if [ ${PUSHGATEWAY_URL} ];then
-        DURATION_BODY+=("${METRICS_NAME}{name=\"${url_temp}\",id=\"${url}\"} ${health_duration_metric}
-        ")
-        STATUS_CODE_BODY+=("${STATUS_CODE_NAME}{name=\"${url_temp}\",id=\"${url}\"} ${health_status_code}
+        BODY+=("${METRICS_NAME}{name=\"${url_temp}\",id=\"${url}\"} ${health_metric}
         ")
     fi
-    echo "${url_temp} ${health_metrics}"
+        echo "${url_temp} ${health_metric}"
 done
 
 if [ ${PUSHGATEWAY_URL} ];then
@@ -79,9 +81,6 @@ echo "Start send metrics to prometheus push gateway"
 cat <<EOF | curl --data-binary @- ${PUSHGATEWAY_URL}
 # HELP ${METRICS_NAME}
 # TYPE ${METRICS_NAME} gauge
-${DURATION_BODY[@]}
-# HELP ${STATUS_CODE_NAME}
-# TYPE ${STATUS_CODE_NAME} gauge
-${STATUS_CODE_BODY[@]}
+${BODY[@]}
 EOF
 fi
